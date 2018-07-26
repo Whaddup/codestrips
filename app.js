8c764d062a73d5bf8ee986b2fef2b5c656bb6247
Whaddup <simonwong163@gmail.com>
2018-07-26 00:42:00 -0400

    fifth commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite')
+
 const PORT = process.env.PORT || 4001;
 
 app.use(bodyParser.json());
@@ -15,13 +18,16 @@ app.listen(PORT, () => {
 
 
 app.get("/strips", (req, res, next) => {
-
   db.all("SELECT * FROM Strip", (error, x) => {
+    if (error){
+      res.status(404).send()
+    }
+    else {
     console.log(x)
     res.send({strips: x})
+    }
   })
 })
 
-  
 
 modules.exports = app;
